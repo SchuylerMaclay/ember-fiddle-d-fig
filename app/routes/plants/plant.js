2ab88219ea722623a3f16ab3a6e0ea558a374a92
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-18 12:20:43 -0500

    add plant profile

--- app/routes/plants/plant.js
+++ app/routes/plants/plant.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+//   renderTemplate: function () {
+//     this.render('plants.plant', {
+//       into: 'plant'
+//     });
+//   },
+//   model: function(params) {
+//     return this.store.find( 'plant' , params.plant_id);
+//   }
 });

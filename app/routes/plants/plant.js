f22f039b894e0f208fe0aa2d4062c0dcce4659a6
Skitelman <sghitelman@gmail.com>
2016-02-18 15:07:08 -0500

    D

--- app/routes/plants/plant.js
+++ app/routes/plants/plant.js
@@ -1,12 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-//   renderTemplate: function () {
-//     this.render('plants.plant', {
-//       into: 'plant'
-//     });
-//   },
-//   model: function(params) {
-//     return this.store.find( 'plant' , params.plant_id);
-//   }
+  actions: {
+    delete(plant) {
+      plant.destroyRecord();
+      this.transitionTo('plants.index');
+    }
+  }
 });

b8e110124dd979a2f4b2e2eb456217475631b182
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-22 14:45:34 -0500

    remove duplicate fonts

--- config/environment.js
+++ config/environment.js
@@ -23,7 +23,7 @@ module.exports = function(environment) {
   };
 
   ENV['ember-simple-auth'] = {
-    routeAfterAuthentication: 'plants',
+    routeAfterAuthentication: 'dashboard',
     routeIfAlreadyAuthenticated: 'plants'
   };
 

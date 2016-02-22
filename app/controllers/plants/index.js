f7ed1100554f7ec96519083032bb962cdd50667c
littlejeon <jeon.lisa@gmail.com>
2016-02-22 16:55:14 -0500

    draggable working

---
+++ app/controllers/plants/index.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  dragFinishText: false,
+  dragStartedText: false,
+  dragEndedText: false,
+  myObject:{id:1, name:'objectName'},
+
+  actions: {
+    dragResult: function(plant, ops) {
+      let user = this.controllerFor('application').get('currentUser');
+      let userPlant = this.store.createRecord('user-plant', {
+        ownership: 'garden',
+        user: user,
+        plant: plant,
+        name: user.get('name') + plant.get('name')
+      });
+      userPlant.save();
+      console.log(userPlant.get('name'));
+      // this.set('dragFinishText', ops.target.resultText);
+      // console.log('Content of draggable-object :',obj);
+    },
+    dragStart: function() {
+      this.set('dragEndedText', false);
+      this.set('dragStartedText','Drag Has Started');
+    },
+    dragEnd: function() {
+
+      this.set('dragStartedText', false);
+      this.set('dragEndedText','Drag Has Ended');
+    },
+    draggingOverTarget: function() {
+      $('#target').css('background-color','red');
+    },
+    leftDragTarget: function() {
+      $('#target').css('background-color','purple');
+    }
+
+  }
+});

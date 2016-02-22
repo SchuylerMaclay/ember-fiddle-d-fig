1a0238457628c9d3412f9bc077d108c726abde73
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-22 13:09:08 -0500

    users working

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // currentUser: null,
   session: Ember.inject.service('session'),
   actions: {
     logout(){
       this.get('session').invalidate();
+    },
+
+  },
+  updateCurrentUser: function () {
+    if (this.get("session.isAuthenticated")){
+      debugger;
+      this.get('store').findRecord('user', 'me').then((user) => {
+        this.set('currentUser', user);
+        console.log(user);
+      });
     }
-  }
+  }.on('init')
 });

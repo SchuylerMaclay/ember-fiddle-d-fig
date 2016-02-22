ee191bc9a26f07bedcf41b8a8cf5100003c08d31
Skitelman <sghitelman@gmail.com>
2016-02-22 17:13:11 -0500

    cancel destroys new model

--- app/controllers/plants/new.js
+++ app/controllers/plants/new.js
@@ -3,6 +3,7 @@ import PlantsBaseController from './base';
 export default PlantsBaseController.extend({
   actions: {
     cancel() {
+      this.model.destroyRecord();
       this.transitionToRoute('plants');
       return false;
     }

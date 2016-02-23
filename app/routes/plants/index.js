9233e7670043e2398264cc91dae59c9fb7b5f25a
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-23 00:23:17 -0500

    still draggin

--- app/routes/plants/index.js
+++ app/routes/plants/index.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend( {
+
+
   model(){
-    return this.store.findAll('plant');
+    let model = this.store.findAll('plant');
+    this.controllerFor('application').set('plants', model);
+    return model;
   }
 });

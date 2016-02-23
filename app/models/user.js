891c46a89432e150fcd7b099faa9c878abfe7236
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-23 13:01:17 -0500

    computes gardens and wishlists on user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,17 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
   userPlants: DS.hasMany('userPlants', { async: true }),
-  reviews: DS.hasMany('reviews', { async: true })
+  reviews: DS.hasMany('reviews', { async: true }),
+
+  garden: Ember.computed.filter('userPlants.@each.ownership', function(userPlant){
+      return userPlant.get('ownership') === "garden";
+  }),
+  wishlist: Ember.computed.filter('userPlants.@each.ownership', function(userPlant){
+      return userPlant.get('ownership') === "wishlist";
+  }),
+
 });

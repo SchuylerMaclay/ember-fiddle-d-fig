9648cb4e8bb687df94100b7ae1b36110d0734f02
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-22 20:03:09 -0500

    init

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,17 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
   userPlants: DS.hasMany('userPlants', { async: true }),
-  reviews: DS.hasMany('reviews', { async: true })
+  reviews: DS.hasMany('reviews', { async: true }),
+
+
+  garden: Ember.computed("userPlants", function(){
+  return this.get("userPlants").filter(function(item){
+    debugger
+    return (item.get('ownership') === "garden");
+  }, this);
+})
 });
